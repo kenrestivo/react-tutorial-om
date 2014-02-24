5b0ead3007d06e903d29266bee7d99e0e2955a5e
ken restivo <ken@restivo.org>
2014-02-24 02:35:19 -0800

    Notes on the piggieback sync issue, etc.

--- project.clj
+++ project.clj
@@ -14,15 +14,24 @@
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [secretary "0.4.0"]
                  [markdown-clj "0.9.40"]
-                 [weasel "0.1.0"] ;; TODO: move to dev
-                 [com.cemerick/piggieback "0.1.3"] ;; TODO: move to dev
                  [cljs-http "0.1.2"]
-                 [om "0.1.7"]]
+                 [om "0.1.7"]
+                 ;; TODO: move to dev
+                 [weasel "0.1.0"] 
+                 [com.cemerick/piggieback "0.1.3"]]
 
+  ;; TODO: also move to dev
   :plugins [[lein-cljsbuild "1.0.0"]
             [lein-ring "0.8.7"]]
 
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  ;; There does not appear to be way to specify versions for nrepl-middleware?
+  ;; But piggieback/wrap-cljs-repl depends on a particular version (0.1.3)
+  ;; of piggieback in dependencies. If piggieback gets bumped to 0.1.4,
+  ;; the middleware will get bumped automatically, and this will likely break,
+  ;; requiring a manual change to dependencies here, and cause
+  ;; strange failures (and did, for me, when using 0.1.2, just now).
+  
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]} ;; TODO: move to dev
   
   :ring {:handler react-tutorial-om.core/app
          :init    react-tutorial-om.core/init}
--- src/clj/react_tutorial_om/repl.clj
+++ src/clj/react_tutorial_om/repl.clj
@@ -3,7 +3,11 @@
             cemerick.piggieback))
 
 (comment
-
+  ;; This whole NS will need a place to live,
+  ;; but if this piggieback and weasel are only loaded for dev,
+  ;; then it'll cause compilation errors if this is here.
+  ;; somehow this ns will need to be condidionally compiled only when in dev mode
+  
  (cemerick.piggieback/cljs-repl :repl-env
                                  (weasel.repl.websocket/repl-env :ip "0.0.0.0" :port 9001))
                                             
